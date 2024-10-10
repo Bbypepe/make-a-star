2b6b18dfa2033e96515eb61e4ee8e5258bcce829
Bbypepe <babypepeofficially@gmail.com>
2024-10-10 08:33:06 +0000

    Saving in-progress work

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'make-a-star',
+  location: 'us-central1'
+};
+
